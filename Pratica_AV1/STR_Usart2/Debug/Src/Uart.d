a2325c8200b242ecd46711aea40ff1517f565a78
Rafael Nogueira Serpa <rafaeln.serpa@gmail.com>
2024-03-20 21:51:52 -0300

    AV1 Pratica

---
+++ Pratica_AV1/STR_Usart2/Debug/Src/Uart.d
@@ -0,0 +1,10 @@
+Src/Uart.o: ../Src/Uart.c ../Inc/Uart.h ../Inc/stm32f4xx.h \
+ ../Inc/core_cm4.h ../Inc/core_cmInstr.h ../Inc/core_cmFunc.h \
+ ../Inc/core_cm4_simd.h ../Inc/system_stm32f4xx.h
+../Inc/Uart.h:
+../Inc/stm32f4xx.h:
+../Inc/core_cm4.h:
+../Inc/core_cmInstr.h:
+../Inc/core_cmFunc.h:
+../Inc/core_cm4_simd.h:
+../Inc/system_stm32f4xx.h:
---
+++ Pratica_AV1/STR_Usart2/Src/Uart.c
@@ -0,0 +1,81 @@
+/*
+ * Uart.c
+ *
+ *  Created on: Mar 11, 2024
+ *      Author: rafae
+ */
+
+#include "Uart.h"
+
+/*
+ * PA9 Alternate functiom
+ * PA9 -> |()
+ */
+
+
+void uart_int(uint32_t BaudRate)
+{
+	RCC->AHB1ENR |= (1U<<0);
+
+	GPIOA->MODER &=~ (1U<<18);
+	GPIOA->MODER |= (1U<<19);
+
+	GPIOA->MODER &=~ (1U<<20);
+	GPIOA->MODER |= (1U<<21);
+
+	GPIOA->AFR[1] |=(1U<<8);
+	GPIOA->AFR[1] |=(1U<<9);
+	GPIOA->AFR[1] |=(1U<<10);
+	GPIOA->AFR[1] &=~(1U<<11);
+
+	GPIOA->AFR[1] |=(1U<<4);
+	GPIOA->AFR[1] |=(1U<<5);
+	GPIOA->AFR[1] |=(1U<<6);
+	GPIOA->AFR[1] &=~(1U<<7);
+
+
+	RCC->APB2ENR |= (1U<<4);
+
+
+	USART1->BRR = compute_uart_bd(sys_freq, BaudRate);
+	NVIC_EnableIRQ(USART1_IRQn);
+	USART1->CR1 = CR1_TE|(1U<<2);
+	USART1->CR1|= CR1_UE;
+	USART1->CR1 |= (1U<<5); //RXNEINE
+
+}
+
+
+
+static uint16_t compute_uart_bd(uint32_t periph_clock,uint32_t Baudrate)
+{
+	return ((periph_clock+(Baudrate/2U))/Baudrate);
+
+}
+
+void uart_write(int ch)
+{
+	while(!(USART1->SR & SR_TXE)){}
+
+		USART1->DR = (ch & 0xFF);
+
+
+}
+char uart_read()
+{
+	while(USART1->SR & SR_RXE){}
+	return USART1->DR;
+}
+
+
+void USART1_IRQHandler()
+{
+	if(USART1->SR & SR_RXE)
+	{
+		char key = USART1->DR;
+		printf("tecla precionada = %c\n\r",key);
+
+	}
+
+
+}
